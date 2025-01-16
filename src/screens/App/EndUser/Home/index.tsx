22a705f209899d7797661fc34eaba1b2625b86fa
Hamza Zahid <hamza.zahid@theappguys.com>
2025-01-16 15:28:35 +0500

    imported review modal

--- src/screens/App/EndUser/Home/index.tsx
+++ src/screens/App/EndUser/Home/index.tsx
@@ -1,40 +1,58 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { PFColors, PFFontSize, PFFonts, appIcons } from '../../../../shared/exporter'
-import { MainWrapper } from '../../../../components'
-import { useSelector } from 'react-redux'
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  PFColors,
+  PFFontSize,
+  PFFonts,
+  appIcons,
+} from '../../../../shared/exporter';
+import {MainWrapper, ReviewModal} from '../../../../components';
+import {useSelector} from 'react-redux';
 
-const Home = ({ navigation }) => {
-    const loginUser = useSelector(state => state?.auth?.loginUser);
-    return (
-        <MainWrapper >
-            <View style={styles.headderContainer}>
-                <Text style={styles.homeText}>Home</Text>
-                <TouchableOpacity onPress={() => navigation.navigate("Settings")}>
-                    <Image source={appIcons.settingIcon} style={styles.settingIcon} resizeMode='contain' />
-                </TouchableOpacity>
-            </View>
-        </MainWrapper>
-    )
-}
+const Home = ({navigation}) => {
+  const [openModal, setOpenModal] = useState(false);
+  const loginUser = useSelector(state => state?.auth?.loginUser);
+  useEffect(() => {
+    // setOpenModal(true);
+  }, [navigation]);
+  return (
+    <MainWrapper>
+      <View style={styles.headderContainer}>
+        <Text style={styles.homeText}>Home</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+          <Image
+            source={appIcons.settingIcon}
+            style={styles.settingIcon}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
+      <ReviewModal
+        modalVisible={openModal}
+        onPressCross={() => setOpenModal(false)}
+        onPressDone={() => setOpenModal(false)}
+      />
+    </MainWrapper>
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    headderContainer: {
-        backgroundColor: PFColors.Standard.White,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingHorizontal: 30,
-        alignItems: "center"
-    },
-    settingIcon: {
-        height: 32,
-        width: 32
-    },
-    homeText: {
-        fontFamily: PFFonts.Foundation.Regular,
-        fontSize: PFFontSize.FONT_SIZE_16,
-        color: PFColors.Standard.Black
-    }
-})
+  headderContainer: {
+    backgroundColor: PFColors.Standard.White,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
+    alignItems: 'center',
+  },
+  settingIcon: {
+    height: 32,
+    width: 32,
+  },
+  homeText: {
+    fontFamily: PFFonts.Foundation.Regular,
+    fontSize: PFFontSize.FONT_SIZE_16,
+    color: PFColors.Standard.Black,
+  },
+});
