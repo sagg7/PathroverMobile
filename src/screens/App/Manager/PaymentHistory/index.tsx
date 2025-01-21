69b5fd720fbc373dc34f4d37d98d6b6e2f7d2e9f
Hamza Zahid <hamza.zahid@theappguys.com>
2025-01-21 17:46:48 +0500

    added payment history screen

---
+++ src/screens/App/Manager/PaymentHistory/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AppHeader, MainWrapper} from '../../../../components';
+
+const PaymentHistory = () => {
+  return (
+    <MainWrapper>
+      <AppHeader title="Payment History" />
+    </MainWrapper>
+  );
+};
+
+export default PaymentHistory;
---
+++ src/screens/App/Manager/PaymentHistory/styles.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+import {
+  PFColors,
+  PFFonts,
+  PFFontSize,
+  scale,
+} from '../../../../shared/exporter';
+
+const styles = StyleSheet.create({
+  contentView: {
+    marginLeft: scale(16),
+    flex: 1,
+  },
+  noDataView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default styles;
