8c7f77e553fdaaf770d0c67e4fe182974661ec3c
Hamza Zahid <hamza.zahid@theappguys.com>
2025-01-21 16:19:08 +0500

    added add icon support in header

--- src/components/complex/AppHeader/index.tsx
+++ src/components/complex/AppHeader/index.tsx
@@ -15,12 +15,15 @@ interface AppHeaderProps {
   leftIcon?: boolean;
   rightIcon?: boolean;
   clickBackIcon?: () => void;
+  clickRightIcon?: () => void;
   desc?: string;
   subtitle?: string;
+  isLeftAddIcon?: boolean;
 }
 const AppHeader: React.FC<AppHeaderProps> = ({
   title,
   clickBackIcon,
+  clickRightIcon,
   leftIcon = true,
   rightIcon = false,
   subtitle,
@@ -45,7 +48,12 @@ const AppHeader: React.FC<AppHeaderProps> = ({
         <Text style={styles.textStyle}>{title}</Text>
 
         {rightIcon ? (
-          <TouchableOpacity activeOpacity={0.7} onPress={() => {}}>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => {
+              clickRightIcon();
+            }}>
+            {svgIcon.Add}
             {/* {svgIcon.NotifyIcon} */}
           </TouchableOpacity>
         ) : (
