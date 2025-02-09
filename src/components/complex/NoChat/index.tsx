fa03c1f2384f8a59e386a34f7221a3c70c6a9700
Hamza Zahid <hamza.zahid@theappguys.com>
2025-02-10 01:51:55 +0500

    design no chat screen

---
+++ src/components/complex/NoChat/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {AppButton} from '../AppButton';
+import {PFColors, PFFonts, PFFontSize} from '../../../shared/exporter';
+
+interface NoChatProps {
+  title: string;
+  subTitle?: string;
+  buttonTitle?: string;
+  onPress?: () => void;
+}
+
+const NoChat = ({title, subTitle, buttonTitle, onPress}: NoChatProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleStyle}>{title}</Text>
+      {subTitle && <Text style={styles.subTitleStyle}>{subTitle}</Text>}
+      {buttonTitle && (
+        <AppButton
+          title={buttonTitle}
+          handleClick={onPress}
+          textStyle={styles.buttonText}
+          buttonStyle={styles.buttonStyle}
+        />
+      )}
+    </View>
+  );
+};
+
+export {NoChat};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.9,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    width: '34%',
+    height: 44,
+  },
+  titleStyle: {
+    color: PFColors.Standard.Black,
+    fontSize: PFFontSize.FONT_SIZE_20,
+    fontFamily: PFFonts.Foundation.SemiBold,
+    textAlign: 'center',
+  },
+  subTitleStyle: {
+    color: PFColors.Gray.LightGray,
+    fontSize: PFFontSize.FONT_SIZE_16,
+    fontFamily: PFFonts.Foundation.Regular,
+    textAlign: 'center',
+    marginVertical: 18,
+  },
+  buttonText: {
+    color: PFColors.Standard.White,
+    fontSize: PFFontSize.FONT_SIZE_10,
+    fontFamily: PFFonts.Foundation.Regular,
+  },
+});
