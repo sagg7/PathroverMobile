86136aae396c809cb892fa234d2967d9d9abb6b9
Hamza Zahid <hamza.zahid@theappguys.com>
2025-02-10 03:08:07 +0500

    added chat screen flow in chat screen

---
+++ src/screens/App/EndUser/Chat/styles.tsx
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+import {PFColors, PFFonts, PFFontSize} from '../../../../shared/exporter';
+
+const styles = StyleSheet.create({
+  
+});
+
+export default styles;
