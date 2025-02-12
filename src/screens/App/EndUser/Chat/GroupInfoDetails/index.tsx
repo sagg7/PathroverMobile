de41f4380a420dc0e642a9ba80760beafe8c6b9c
Hamza Zahid <hamza.zahid@theappguys.com>
2025-02-12 21:28:44 +0500

    styling fixes, show avatars

--- src/screens/App/EndUser/Chat/GroupInfoDetails/index.tsx
+++ src/screens/App/EndUser/Chat/GroupInfoDetails/index.tsx
@@ -1,13 +1,17 @@
 import {View, Text, TouchableOpacity, Image, FlatList} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {MainWrapper} from '../../../../../components';
 import styles from './styles';
 import {svgIcon} from '../../../../../assets/svg';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {useIsFocused, useNavigation, useRoute} from '@react-navigation/native';
 import {appIcons} from '../../../../../assets/icons';
-import {useExitGroupMutation} from '../../../../../redux/chat/chatApiSlice';
+import {
+  useExitGroupMutation,
+  useGetGroupInfoMutation,
+} from '../../../../../redux/chat/chatApiSlice';
+import {showAlert} from '../../../../../shared/exporter';
 
-const GroupButtons = () => {
+const GroupButtons = ({onPressAdd}) => {
   return (
     <View style={styles.buttonView}>
       <TouchableOpacity style={styles.buttonStyle}>
@@ -18,7 +22,7 @@ const GroupButtons = () => {
         {svgIcon.VideoIcon}
         <Text style={styles.buttonText}>Video</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.buttonStyle}>
+      <TouchableOpacity style={styles.buttonStyle} onPress={onPressAdd}>
         {svgIcon.PlusIcon}
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
@@ -28,12 +32,37 @@ const GroupButtons = () => {
 
 const GroupInfoDetail = () => {
   const {params} = useRoute();
+  const isFocused = useIsFocused();
   const navigation = useNavigation();
-  const [exitGroup] = useExitGroupMutation();
+  const [exitGroup, {isError, error}] = useExitGroupMutation();
+  const [getGroupInfo, {isLoading, data}] = useGetGroupInfoMutation();
 
-  console.log('==============params======================');
-  console.log(params);
-  console.log('====================================');
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      if (isFocused && params?.item) {
+        await getGroupInfo(params?.item?.id);
+      }
+    })();
+  }, [isFocused, params]);
+
+  useEffect(() => {
+    if (error) {
+      showAlert(
+        'Error',
+        error?.data?.error ||
+          error?.data?.success ||
+          'Unable to process request. Please try again!.',
+      );
+    }
+  }, [isError]);
+
+  useEffect(() => {
+    if (data) {
+      setMembers(data);
+    }
+  }, [data]);
 
   const onPressExit = async () => {
     try {
@@ -52,25 +81,46 @@ const GroupInfoDetail = () => {
 
   const listFooterItem = () => {
     return (
-      <TouchableOpacity style={styles.footerView} onPress={onPressExit}>
-        {svgIcon.Exit}
-        <Text style={styles.redText}>Exit Group</Text>
-      </TouchableOpacity>
+      !members?.is_exit && (
+        <TouchableOpacity style={styles.footerView} onPress={onPressExit}>
+          {svgIcon.Exit}
+          <Text style={styles.redText}>Exit Group</Text>
+        </TouchableOpacity>
+      )
     );
   };
 
   const renderItem = ({item, index}) => {
     return (
       <TouchableOpacity style={styles.userContainer} onPress={() => {}}>
-        <Image source={appIcons.userPlaceholder} style={styles.imageStyle} />
+        <Image
+          source={
+            item?.avatar_url
+              ? {uri: item?.avatar_url}
+              : appIcons.userPlaceholder
+          }
+          style={styles.imageStyle}
+        />
         <View style={styles.textView}>
-          <Text style={styles.nameText}>{item?.name || 'John' || ''}</Text>
+          <Text style={styles.nameText}>
+            {item?.first_name || item?.last_name
+              ? `${item?.first_name || ''} ${item?.last_name || ''}`.trim()
+              : 'User'}
+          </Text>
           <Text style={styles.adminText}>
-            {index === 0 ? 'Group Admin' : ''}
+            {item?.is_admin ? 'Group Admin' : ''}
           </Text>
         </View>
       </TouchableOpacity>
     );
+  };
+
+  const onPressAdd = () => {
+    navigation.navigate('MemberList', {
+      isAdd: true,
+      item: params?.item,
+      members: members?.users,
+    });
   };
 
   return (
@@ -98,10 +148,10 @@ const GroupInfoDetail = () => {
         />
         <Text style={styles.headerText}>{params?.item?.name || ''}</Text>
         <Text style={styles.subHeaderText}>Group| 8 Members</Text>
-        <GroupButtons />
+        <GroupButtons onPressAdd={onPressAdd} />
       </View>
       <FlatList
-        data={[1, 2, 3, 4, 5, 6.7]}
+        data={members?.users}
         renderItem={renderItem}
         ListFooterComponent={listFooterItem}
         showsVerticalScrollIndicator={false}
