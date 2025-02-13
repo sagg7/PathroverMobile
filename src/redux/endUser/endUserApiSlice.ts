b1ff17c1dad98e7d2a1c20b430bef63d11e1f15d
Hamza Zahid <hamza.zahid@theappguys.com>
2025-02-13 18:08:16 +0500

    in progress

--- src/redux/endUser/endUserApiSlice.ts
+++ src/redux/endUser/endUserApiSlice.ts
@@ -3,10 +3,14 @@ import {apiSlice} from '../api/apiSlice';
 export const enduserApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     getAllWells: builder.query({
-      query: () => ({
-        url: 'user_routes/well_location',
-        method: 'GET',
-      }),
+      query: ({...params}) => {
+        const queryParams = new URLSearchParams({...params}).toString();
+        console.log('QUERY PARAM', queryParams);
+        return {
+          url: `user_routes/well_location?${queryParams}`,
+          method: 'GET',
+        };
+      },
     }),
     getAllSaveRoutes: builder.query({
       query: ({type, ...params}) => {
