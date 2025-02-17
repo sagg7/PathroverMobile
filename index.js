ad374f7a2b7d0390b7b964dec90ad697f332d77a
Hamza Zahid <hamza.zahid@theappguys.com>
2025-02-17 14:40:59 +0500

    disabled text & textinput font scaling

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Settings, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+Settings.set({fontScaling: false});
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+TextInput.defaultProps = Text.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
