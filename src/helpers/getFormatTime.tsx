21077b7936d1db5f2cb71d385904c8fbb68f06db
Hamza Zahid <hamza.zahid@theappguys.com>
2025-02-27 15:43:03 +0500

    helper for formatting time

---
+++ src/helpers/getFormatTime.tsx
@@ -0,0 +1,13 @@
+// Function to format time in hh:mm:ss
+export const formatTime = milliseconds => {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  // Pad with leading zeros
+  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(
+    2,
+    '0',
+  )}:${String(seconds).padStart(2, '0')}`;
+};
