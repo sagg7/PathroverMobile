78c0e04e849b63984cf5c259af3f3a6b1d9f6635
Hamza Zahid <hamza.zahid@theappguys.com>
2025-03-05 20:19:14 +0500

    added overview text styles

--- src/screens/App/EndUser/LocationDetail/Overview/styles.tsx
+++ src/screens/App/EndUser/LocationDetail/Overview/styles.tsx
@@ -1,9 +1,15 @@
 import { StyleSheet } from "react-native";
+import { PFColors, PFFonts, PFFontSize } from "../../../../../shared/exporter";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    textStyle: {
+         color: PFColors.Gray.DarkGray,
+                fontFamily: PFFonts.Foundation.Regular,
+                fontSize: PFFontSize.FONT_SIZE_12,
+    }
 });
 
 export default styles;
