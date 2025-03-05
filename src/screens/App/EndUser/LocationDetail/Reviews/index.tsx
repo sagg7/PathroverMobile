19be24280b69246a2f1635b37649e556bc5ac454
Hamza Zahid <hamza.zahid@theappguys.com>
2025-03-05 16:22:39 +0500

    reviews screen design

---
+++ src/screens/App/EndUser/LocationDetail/Reviews/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    View
+} from 'react-native';
+import styles from './styles';
+import { Text } from 'react-native';
+
+const Reviews = () => {
+  return (
+    <View style={styles.container}>
+     <Text>Reviews</Text>
+    </View>
+  );
+};
+
+export default Reviews;
