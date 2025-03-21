995806489afd4a9add9f7339a8715dcf0f82a035
Hamza Zahid <hamza.zahid@theappguys.com>
2025-03-21 22:08:30 +0500

    fixed ai chat issue

--- src/redux/chat/chatSlice.ts
+++ src/redux/chat/chatSlice.ts
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
+import { Platform } from 'react-native';
 
 const chatSlice = createSlice({
   name: 'chat',
@@ -16,7 +17,9 @@ const chatSlice = createSlice({
           name: 'bot',
           _id: 2,
         },
-        image: action.payload?.[0]?.image,
+        image: action.payload?.[0]?.attachment?.sourceURL,
+        // image: action.payload?.[0]?.attachment?.sourceURL,
+        // image: Platform.OS === 'ios' ? action.payload?.[0]?.attachment?.sourceURL?.replace('///', '//') : action.payload?.[0]?.attachment?.sourceURL,
       });
     },
     addBotMessage: (state, action) => {
