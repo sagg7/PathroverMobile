c1b99d73335b7ac4dfe44e9a86c011ca3eb68e86
Hamza Zahid <hamza.zahid@theappguys.com>
2025-04-21 14:10:32 +0500

    Added navigation reference screen

--- src/helpers/navigationHelper.tsx
+++
@@ -1,7 +0,0 @@
-import { StackActions } from "@react-navigation/native";
-
-export function replace(name, params) {
-  if (navigationRef.isReady()) {
-    navigationRef?.current?.dispatch(StackActions.replace(name, params));
-  }
-}
