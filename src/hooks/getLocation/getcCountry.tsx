71f95e8ec1a5ccdd81cec598e7c5674c3437a547
Hamza Zahid <hamza.zahid@theappguys.com>
2025-05-01 05:40:08 +0500

    lint fix

--- src/hooks/getLocation/getcCountry.tsx
+++ src/hooks/getLocation/getcCountry.tsx
@@ -1,7 +1,7 @@
 import { mapBoxToken } from "../../shared/exporter";
 
 export async function getCountryFromCoordinates(lat, lng) {
-  const accessToken = mapBoxToken; 
+  const accessToken = mapBoxToken;
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${accessToken}&types=country`;
 
   try {
@@ -9,18 +9,14 @@ export async function getCountryFromCoordinates(lat, lng) {
     const data = await response.json();
 
     if (data.features && data.features.length > 0) {
-      const countryName = data.features[0].text; 
-      const countryCode = data.features[0].properties.short_code.toUpperCase(); 
-      
-      console.log("Country Name:", countryName);
-      console.log("Country Code:", countryCode);
-      
+      const countryName = data.features[0].text;
+      const countryCode = data.features[0].properties.short_code.toUpperCase();
+
       return { countryName, countryCode };
     } else {
-      throw new Error("Country not found");
+      //
     }
   } catch (error) {
-    console.error("Error fetching country:", error);
     return null;
   }
 }
